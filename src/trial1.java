efecd487709be9d0012cf773dedbb61d1d404076
Shifra Goldstone <sgongithub@gmail.com>
2018-10-04 10:14:20 -0400

    This is the first commit

---
+++ src/trial1.java
@@ -0,0 +1,11 @@
+
+public class trial1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, this is a trial run.");
+		System.out.println("Let's add the numbers 5 and 6");
+		int number = addNums();
+		System.out.printf("The sum is %d", number);
+	}
+}
